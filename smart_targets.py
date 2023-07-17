964658eb05531a9d972cb91f505d38c1471ff1db
Ewelina Nicholson <e.guzikk@gmail.com>
2023-07-17 18:51:46 +0100

    Smart targets

---
+++ smart_targets.py
@@ -0,0 +1,38 @@
+
+
+# Task 1:
+# Create a variable and assign it the value of "hello"
+# Use an if statement to check if the variable is equal to hello, and if it is print"Hey there!" 
+
+greeting = "Hello"
+
+if greeting == "Hello":
+
+    print("Hey there!")
+
+
+# Create a dictionary with the keys of "brand" with the value of "nike" and the key of "type" and the value of "shoe". 
+# Access from the dictionary and print out the value held under brand key.
+
+trainers = { "brand": "Nike",
+             "type": "shoe"}
+
+print(trainers["brand"])
+
+
+# Task 2:
+# Create a list of numbers from 1 to 10
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Using a loop add two to each number 
+
+for number  in numbers:
+
+    print(number+2)
+    
+# Using a loop print out each number in the list 
+
+for number in numbers:
+    print(number)
+
